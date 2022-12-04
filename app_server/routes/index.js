77cb6bd57820d950f821080889e5406e5793bde8
erkankilic0 <114809658+erkankilic0@users.noreply.github.com>
2022-12-04 23:43:30 +0300

    Update index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -7,7 +7,7 @@ var ctrlDigerleri = require('../controllers/digerleri');
 router.get('/', ctrlMekanlar.anaSayfa);
 router.get('/mekan/:mekanid', ctrlMekanlar.mekanBilgisi);
 router.get('/mekan/yorum/yeni', ctrlMekanlar.yorumEkle);
-router.get('/hakkinda', ctrlDigerleri.hakkinda);
+//router.get('/hakkinda', ctrlMekanlar.mekanBilgisi);
 
 
 module.exports = router;
